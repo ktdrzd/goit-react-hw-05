a9f73b3331cfb48ff7bd6baa5d1fe85a4a4c4c25
ktdrzd <137915871+ktdrzd@users.noreply.github.com>
2024-03-03 23:17:10 +0200

    added API

---
+++ src/api.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+const API_KEY = '35aa8c3124ef4e496a774e7d787c1e36';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const fetchTrendDay = async () => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&language=uk-UA&per_page=10`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const fetchById = async id => {
+  try {
+    const response = await axios.get(`${BASE_URL}/movie?${id}?api_key=${API_KEY}&language=uk-UA`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const fetchInfo = async (id, select) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/movie/${id}/${select}?api_key=${API_KEY}&language=uk-UA'`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const fetchByQuery = async (query) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/search/movie?query=${query}&api_key=${API_KEY}&language=uk-UA&include_adult=false&page=1`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
